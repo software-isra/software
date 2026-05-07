139817783ade24071c3cbcac458e507d815d8b3d
s12216853-create <s12216853@stu.najah.edu>
2026-05-07 11:20:25 +0300

    Update ConsoleNotificationService.java

--- scheduling-system/src/main/java/appointment_system/notification/ConsoleNotificationService.java
+++ scheduling-system/src/main/java/appointment_system/notification/ConsoleNotificationService.java
@@ -1,9 +1,13 @@
 package appointment_system.notification;
 
+import java.util.logging.Logger;
+
 public class ConsoleNotificationService implements NotificationService {
+
+    private static final Logger LOGGER = Logger.getLogger(ConsoleNotificationService.class.getName());
 
     @Override
     public void sendReminder(String recipient, String message) {
-        System.out.println("Reminder sent to " + recipient + ": " + message);
+        LOGGER.info("Reminder sent to " + recipient + ": " + message);
     }
 }
